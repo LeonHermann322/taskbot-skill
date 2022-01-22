84fc5ed62ab8c40fae451029988d73aede85540f
LeonHermann322 <Leon.Hermann@student.hpi.uni-potsdam.de>
2022-01-22 17:06:52 +0100

    general paths and random task selection

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+import shutil
+
+from numpy import imag
 from mycroft import MycroftSkill, intent_handler
 from adapt.intent import IntentBuilder
 import os
@@ -5,14 +8,19 @@ import random
 from PIL import Image
 import requests
 import time
-import glob
 import subprocess
 class Taskbot(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.scoreFile = "/home/leonhermann/.config/mycroft/skills/Judgealexa/scoreFile.txt"
+        self.scoreFile = "./scoreFile.txt"
         if not os.path.exists(self.scoreFile):
-            self.setScore(1000)
+            self.setScore(1100)
+
+        self.labeling_folder = "/tmp/random_pictures"
+        if not os.path.exists(self.labeling_folder):
+            os.mkdir(self.labeling_folder)
+
+        self.animal_dataset = "/opt/mycroft/skills/taskbot-skill/animal_dataset/raw-img"
 
     def getScore(self):
         scoreFile = open(self.scoreFile, "r")
@@ -24,21 +32,10 @@ class Taskbot(MycroftSkill):
         scoreFile = open(self.scoreFile, "w+")
         scoreFile.write(str(points))
         scoreFile.close()
-    
-    def ask_mask_task(self):
-        answer = self.get_response('askMathTask')
-        if self.voc_match(answer, 'yes', self.lang):
-            self.speak("Your answer is correct. Your score has been increased")
-            self.setScore(self.getScore() + 5)
-        else:
-            self.speak("Fuck you the answer is wrong")
-            self.setScore(self.getScore() - 5)
-
 
     @intent_handler(IntentBuilder('score').require('Score'))
     def handle_score(self, message):
         points = self.getScore()
-        self.log.info("Calculating response")
         self.speak(f"Your score is {points}")
 
     @intent_handler(IntentBuilder('deny').require('Deny'))
@@ -46,62 +43,62 @@ class Taskbot(MycroftSkill):
         points = self.getScore()
         answer = self.get_response('serviceDenied')
         if self.voc_match(answer, 'yes', self.lang):
-            self.ask_mask_task()
+            self.handle_task(message)
         elif self.voc_match(answer, 'no', self.lang):
-            self.speak("okay, loser")
-
-    @intent_handler(IntentBuilder('mathTask').require('MathTask'))
-    def handle_math_task(self, message):
-        self.askMathTask()
-
+            self.speak("Okay")
     
-    @intent_handler(IntentBuilder('animalTask').require('AnimalTask'))
-    def handle_animal_task(self, message):
-        self.classify_task()
+    @intent_handler(IntentBuilder('task').require('Task'))
+    def handle_task(self, message):
+        tasks = [self.classify_task, self.verify_task]
+        task = random.choice(tasks)
+        task(message)
 
-   
-            
-    def verify_task(self):
+    def verify_task(self, message):
         animal_list = ["dog", "elephant", "cat", "chicken"]
         current_animal = random.choice(animal_list)
-        folder_path = f"/home/leonhermann/animal_dataset/raw-img/{current_animal}"
+        folder_path = os.path.join(self.animal_dataset, current_animal)
         picture_list = []
         for file in os.listdir(folder_path):
             picture_list.append(file)
-        picture = Image.open(folder_path + "/" + random.choice(picture_list))
-        picture.show()
+        picture_path = os.path.join(folder_path, random.choice(picture_list))
+        viewer = subprocess.Popen(['eog', picture_path])
         answer = self.get_response("What animal do you see ?")
+        viewer.terminate()
         if answer == current_animal:
             self.speak("You are correct. Score increased")
             self.setScore(self.getScore() + 5)
         else:
             self.speak(f"Are you kidding me? This clearly was a {current_animal}. Your Score has been decreased")
             self.setScore(self.getScore() - 20)
-        picture.close()
-        
-    def classify_task(self):
-        folder_path = "/home/leonhermann/random_pictures"
+
+    def download_save_image(self, curr_time):
         path = "https://picsum.photos/800/480.jpg"
-        img = requests.get(path)
+        res = requests.get(path)
+
+        file_path = f"{self.labeling_folder}/{curr_time}.jpg"
+        if res.status_code == 200:
+            with open(file_path, "wb") as image_file:
+                image_file.write(res.content)
+
+        return file_path
+        
+    def classify_task(self, message):
         t = time.localtime()
         current_time = time.strftime("%Y%m%d%H%M%S", t)
-        file_path = f"{folder_path}/{current_time}.jpg"
+        file_path = self.download_save_image(current_time)
 
-        with open(f"{folder_path}/{current_time}.jpg", "wb") as fd:
-            fd.write(img.content)
-        #picture = 
+        viewer = subprocess.Popen(['eog', file_path])
+        answer = self.get_response("What do you see?")
+        if answer:
+            self.speak("Thank you for your work!")
+            viewer.terminate()
+            self.setScore(self.getScore() + 5)
+        else:
+            self.speak("I would like an answer please!")
+            return
+        
+        with open(self.labeling_folder + "/labels.csv", 'a') as label_file:
+            label_file.write(f"{current_time}.jpg,{answer}\n")
 
-        for infile in glob.glob(file_path):
-            viewer = subprocess.Popen(['eog', infile])
-            answer = self.get_response("What do you see?")
-            if answer:
-                self.speak("thank you for your work")
-                viewer.terminate()
-                viewer.kill()
-        
-        with open(folder_path + "/labels.csv", 'a') as fd:
-            fd.write(f"{current_time}.jpg,{answer} \n")
-        
-        
 def create_skill():
     return Taskbot()
